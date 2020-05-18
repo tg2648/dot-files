a4b1e9828f1c0d93a308175c1fe6b064da7933cc
tg2648 <45052145+tg2648@users.noreply.github.com>
2020-05-17 20:04:53 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+chmod 700 dot-files/.*
+
+mv ~/dot-files/.bashrc ~/.my_bashrc
+echo "source ~/.my_bashrc" >> ~/.bashrc
+
+if [ -f ~/.vimrc ]; then
+    echo ".vimrc exist, creating a backup .vimrc_backup"
+    mv ~/.vimrc ~/.vimrc_backup
+fi
+
+mv dot-files/.vimrc ~/.vimrc
+
+echo "Finished dot file setup"
